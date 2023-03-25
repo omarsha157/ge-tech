fc7d706bbf49346905245d0bb8c04557053d6930
omarsha157 <omarsha157@gmail.com>
2023-03-25 09:17:18 +0400

    firts

---
+++ app.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector('.burger')
+const navlink = document.querySelector('.navlinks')
+const navbar = document.querySelector('nav')
+
+burger.addEventListener('click',() => {
+    navlink.classList.toggle('nav-active')
+    burger.classList.toggle('burger-close')
+})
+
+window.onscroll = () => {
+    scrollY > 500 ? navbar.classList.add('nav-bg') : navbar.classList.remove('nav-bg')
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    //    else {
+    //     reveals[i].classList.remove("active");
+    //   }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
+  // To check the scroll position on page load
+//   reveal();
+
